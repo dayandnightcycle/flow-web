d992a5812dd9999d773b9a71c38e6de9140e1176
comp_lan <lanhedong@126.com>
2023-02-22 17:54:44 +0800

    查询优化

--- src/views/tjfx/zs.vue
+++ src/views/tjfx/zs.vue
@@ -2,47 +2,18 @@
   <div>
     <v-form-render :form-json="formJson" :form-data="formData" :option-data="optionData" ref="vFormRef">
     </v-form-render>
-    <el-dialog v-model="zsdialogVisible" title="不动产追溯" width="70%" :before-close="dialogVisible = false"
-               class="ywdialog">
+    <el-dialog v-model="zsdialogVisible" title="不动产追溯" width="1000px !important" :before-close="dialogVisible = false"
+               class="ywdialog" style="margin-left: 400px;width: 1200px;">
 
 
       <div style="display:flex;flex-direction:column">
-        <el-tree style="display:1" :data="treedata" :props="defaultProps" @node-click="handleNodeClick">
+        <el-tree :data="treeData" :props="defaultProps" @node-click="handleNodeClick">
           <template #default="{ data }">
             <span :class="data.icon" :style="data.style"></span>
             {{ data.label }}
           </template>
         </el-tree>
-        <div
-            style="position: absolute;bottom: 10px;height: 120px;width: 95%;margin: 0px auto;border: 1px solid #d3d1d1;display:flex;flex-direction: column;"
-            v-if='lcmsVisiable'>
-          <div style="height:30px;width:100%;display:flex;">
-            <div
-                style="width:90px;border-right:1px solid #d3d1d1;line-height: 30px;font-size: 12px;font-weight:600;color:#000;padding-left:10px">
-              流程期限：
-            </div>
-            <div style="display:flex;line-height: 30px;">
-              <el-input style="width:400px" v-model="inputday" placeholder="请输入时间" clearable/>
-              天
-              <el-select v-model="gzrvalue" class="m-2" style="width:100px">
-                <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value"/>
-              </el-select>
 
-            </div>
-          </div>
-          <div style="height:90px;width:100%;display: flex;">
-            <div
-                style="width:90px;border-right:1px solid #d3d1d1;line-height: 90px;font-size: 12px;font-weight:600;color:#000;padding-left:10px">
-              流程描述：
-            </div>
-            <div style="display:flex;">
-              <div style="width:400px;border-right:1px solid #d3d1d1"></div>
-              <el-button style="width: 90px;height: 70px;margin-left: 15px;margin-top: 10px;"
-                         @click="CreateProcess">创建流程
-              </el-button>
-            </div>
-          </div>
-        </div>
       </div>
 
 
@@ -63,7 +34,14 @@ const zs = () => {
   vFormRef.value.setFormJson(zsjson);
 
 }
-
+//树结构数据
+const treeData = ref();
+const defaultProps = {
+  children: 'children',
+  label: 'label',
+}
+const handleNodeClick = (data) => {
+}
 const query = () => {
 
   setTimeout(() => {
@@ -126,22 +104,41 @@ const query = () => {
       }
 
       var data = {
-        query: "SELECT * FROM (SELECT ROWNUM AS RN, t1.* FROM (SELECT DISTINCT A.SLBH,A.DJLX,A.BDCZH,B.BDCDYH,A.ZSXLH,A.DJRQ,(select wm_concat(distinct to_char(y.qlrmc)) from dj_qlrgl x left join dj_qlr y on x.qlrid = y.qlrid where x.slbh = a.slbh and x.qlrlx = '权利人') as QLRMC,(select wm_concat(distinct to_char(y.zjhm)) from dj_qlrgl x left join dj_qlr y on x.qlrid = y.qlrid where x.slbh = a.slbh and x.qlrlx = '权利人') as QLRZJH,N'' AS ZT,A.GDH,NVL(C.ZL, C.TZRTXDZ) AS ZL,A.LIFECYCLE,A.YWLX,B.TSTYBM FROM (SELECT SLBH,DJLX,BDCZH,ZSXLH,DJRQ,LIFECYCLE,N'权证' AS YWLX,GDH FROM DJ_DJB UNION ALL SELECT SLBH,DJLX,BDCZMH AS BDCZH,ZSXLH,DJRQ,LIFECYCLE,N'预告' AS YWLX,GDH FROM DJ_YG UNION ALL SELECT A.SLBH,A.DJLX,A.BDCZMH AS BDCZH,A.ZSXLH,A.DJRQ,A.LIFECYCLE,N'抵押' AS YWLX,A.GDH FROM DJ_DY A LEFT JOIN DJ_TSGL B ON A.SLBH = B.SLBH LEFT JOIN DJ_XGDJGL C ON A.SLBH = C.ZSLBH WHERE A.XGZH IS NULL AND C.XGZH IS NULL AND C.FSLBH IS NULL UNION ALL SELECT A.SLBH,A.CFLX AS DJLX,A.CFBH AS BDCZH,N'' AS ZSXLH,A.DJSJ AS DJRQ,A.LIFECYCLE,N'查封' AS YWLX,A.GDH FROM DJ_CF A LEFT JOIN DJ_TSGL B ON A.SLBH = B.SLBH LEFT JOIN DJ_XGDJGL C ON A.SLBH = C.ZSLBH WHERE A.XGZH IS NULL AND C.XGZH IS NULL AND C.FSLBH IS NULL) A LEFT JOIN DJ_TSGL B ON A.SLBH = B.SLBH LEFT JOIN DJ_SJD C ON A.SLBH = C.SLBH WHERE B.TSTYBM IS NOT NULL " + getWhere() + " order by A.Bdczh desc, A.SLBH) t1)WHERE RN >= 1 AND RN <= 20 ORDER BY ROWNUM ASC",
-        count: "SELECT COUNT(0) FROM (SELECT DISTINCT A.SLBH,A.DJLX,A.BDCZH,B.BDCDYH,A.ZSXLH,A.DJRQ,(select wm_concat(distinct to_char(y.qlrmc)) from dj_qlrgl x left join dj_qlr y on x.qlrid = y.qlrid where x.slbh = a.slbh and x.qlrlx = '权利人') as QLRMC,(select wm_concat(distinct to_char(y.zjhm)) from dj_qlrgl x left join dj_qlr y on x.qlrid = y.qlrid where x.slbh = a.slbh and x.qlrlx = '权利人') as QLRZJH,N'' AS ZT,A.GDH,NVL(C.ZL, C.TZRTXDZ) AS ZL,A.LIFECYCLE,A.YWLX,B.TSTYBM FROM (SELECT SLBH,DJLX,BDCZH,ZSXLH,DJRQ,LIFECYCLE,N'权证' AS YWLX,GDH FROM DJ_DJB UNION ALL SELECT SLBH,DJLX,BDCZMH AS BDCZH,ZSXLH,DJRQ,LIFECYCLE,N'预告' AS YWLX,GDH FROM DJ_YG UNION ALL SELECT A.SLBH,A.DJLX,A.BDCZMH AS BDCZH,A.ZSXLH,A.DJRQ,A.LIFECYCLE,N'抵押' AS YWLX,A.GDH FROM DJ_DY A LEFT JOIN DJ_TSGL B ON A.SLBH = B.SLBH LEFT JOIN DJ_XGDJGL C ON A.SLBH = C.ZSLBH WHERE A.XGZH IS NULL AND C.XGZH IS NULL AND C.FSLBH IS NULL UNION ALL SELECT A.SLBH,A.CFLX AS DJLX,A.CFBH AS BDCZH,N'' AS ZSXLH,A.DJSJ AS DJRQ,A.LIFECYCLE,N'查封' AS YWLX,A.GDH FROM DJ_CF A LEFT JOIN DJ_TSGL B ON A.SLBH = B.SLBH LEFT JOIN DJ_XGDJGL C ON A.SLBH = C.ZSLBH WHERE A.XGZH IS NULL AND C.XGZH IS NULL AND C.FSLBH IS NULL) A LEFT JOIN DJ_TSGL B ON A.SLBH = B.SLBH LEFT JOIN DJ_SJD C ON A.SLBH = C.SLBH WHERE B.TSTYBM IS NOT NULL " + getWhere() + " order by A.Bdczh desc, A.SLBH) t"
+        //有权利人名称和证件号
+        // query: "SELECT * FROM (SELECT ROWNUM AS RN, t1.* FROM (SELECT DISTINCT A.SLBH,A.DJLX,A.BDCZH,B.BDCDYH,A.ZSXLH,A.DJRQ,(select wm_concat(distinct to_char(y.qlrmc)) from dj_qlrgl x left join dj_qlr y on x.qlrid = y.qlrid where x.slbh = a.slbh and x.qlrlx = '权利人') as QLRMC,(select wm_concat(distinct to_char(y.zjhm)) from dj_qlrgl x left join dj_qlr y on x.qlrid = y.qlrid where x.slbh = a.slbh and x.qlrlx = '权利人') as QLRZJH,N'' AS ZT,A.GDH,NVL(C.ZL, C.TZRTXDZ) AS ZL,A.LIFECYCLE,A.YWLX,B.TSTYBM FROM (SELECT SLBH,DJLX,BDCZH,ZSXLH,DJRQ,LIFECYCLE,N'权证' AS YWLX,GDH FROM DJ_DJB UNION ALL SELECT SLBH,DJLX,BDCZMH AS BDCZH,ZSXLH,DJRQ,LIFECYCLE,N'预告' AS YWLX,GDH FROM DJ_YG UNION ALL SELECT A.SLBH,A.DJLX,A.BDCZMH AS BDCZH,A.ZSXLH,A.DJRQ,A.LIFECYCLE,N'抵押' AS YWLX,A.GDH FROM DJ_DY A LEFT JOIN DJ_TSGL B ON A.SLBH = B.SLBH LEFT JOIN DJ_XGDJGL C ON A.SLBH = C.ZSLBH WHERE A.XGZH IS NULL AND C.XGZH IS NULL AND C.FSLBH IS NULL UNION ALL SELECT A.SLBH,A.CFLX AS DJLX,A.CFBH AS BDCZH,N'' AS ZSXLH,A.DJSJ AS DJRQ,A.LIFECYCLE,N'查封' AS YWLX,A.GDH FROM DJ_CF A LEFT JOIN DJ_TSGL B ON A.SLBH = B.SLBH LEFT JOIN DJ_XGDJGL C ON A.SLBH = C.ZSLBH WHERE A.XGZH IS NULL AND C.XGZH IS NULL AND C.FSLBH IS NULL) A LEFT JOIN DJ_TSGL B ON A.SLBH = B.SLBH LEFT JOIN DJ_SJD C ON A.SLBH = C.SLBH WHERE B.TSTYBM IS NOT NULL " + getWhere() + " order by A.Bdczh desc, A.SLBH) t1)WHERE RN >= 1 AND RN <= 20 ORDER BY ROWNUM ASC",
+        //没有权利人名称和证件号
+        query: "SELECT *  from (SELECT ROWNUM AS RN, t1.*  from (SELECT DISTINCT A.SLBH,A.DJLX,A.BDCZH,B.BDCDYH,A.ZSXLH,A.DJRQ,N''AS QLRMC,N''AS QLRZJH,N''AS ZT,A.GDH,NVL(C.ZL, C.TZRTXDZ) AS ZL,A.LIFECYCLE,A.YWLX,B.TSTYBM   from (SELECT SLBH,DJLX,BDCZH,ZSXLH,DJRQ,LIFECYCLE,N'权证' AS YWLX,GDH   from DJ_DJB UNION ALL SELECT SLBH,DJLX,BDCZMH AS BDCZH,ZSXLH,DJRQ,LIFECYCLE,N'预告'  AS YWLX,GDH   from DJ_YG UNION ALL SELECT A.SLBH,A.DJLX,A.BDCZMH AS BDCZH,A.ZSXLH,A.DJRQ,A.LIFECYCLE,N'抵押'    AS YWLX,A.GDH   from DJ_DY A LEFT JOIN DJ_TSGL B ON A.SLBH = B.SLBH LEFT JOIN DJ_XGDJGL C ON A.SLBH = C.ZSLBH WHERE A.XGZH IS NULL AND C.XGZH IS NULL AND C.FSLBH IS NULL UNION ALL SELECT A.SLBH,A.CFLX AS DJLX,A.CFBH AS BDCZH,N''    AS ZSXLH,A.DJSJ AS DJRQ,A.LIFECYCLE,N'查封'  AS YWLX,A.GDH   from DJ_CF A LEFT JOIN DJ_TSGL B ON A.SLBH = B.SLBH LEFT JOIN DJ_XGDJGL C ON A.SLBH = C.ZSLBH WHERE A.XGZH IS NULL AND C.XGZH IS NULL AND C.FSLBH IS NULL) A LEFT JOIN DJ_TSGL B ON A.SLBH = B.SLBH LEFT JOIN DJ_SJD C ON A.SLBH = C.SLBH WHERE B.TSTYBM IS NOT NULL " + getWhere() + " order by A.Bdczh desc, A.SLBH) t1)WHERE RN >= 1 AND RN <= 20ORDER BY ROWNUM ASC",
+        count: "SELECT COUNT(0) FROM (SELECT DISTINCT A.SLBH,A.DJLX,A.BDCZH,B.BDCDYH,A.ZSXLH,A.DJRQ,N''AS QLRMC,N''AS QLRZJH,N''AS ZT,A.GDH,NVL(C.ZL, C.TZRTXDZ) AS ZL,A.LIFECYCLE,A.YWLX,B.TSTYBM   from (SELECT SLBH,DJLX,BDCZH,ZSXLH,DJRQ,LIFECYCLE,N'权证' AS YWLX,GDH   from DJ_DJB UNION ALL SELECT SLBH,DJLX,BDCZMH AS BDCZH,ZSXLH,DJRQ,LIFECYCLE,N'预告'  AS YWLX,GDH   from DJ_YG UNION ALL SELECT A.SLBH,A.DJLX,A.BDCZMH AS BDCZH,A.ZSXLH,A.DJRQ,A.LIFECYCLE,N'抵押'    AS YWLX,A.GDH   from DJ_DY A LEFT JOIN DJ_TSGL B ON A.SLBH = B.SLBH LEFT JOIN DJ_XGDJGL C ON A.SLBH = C.ZSLBH WHERE A.XGZH IS NULL AND C.XGZH IS NULL AND C.FSLBH IS NULL UNION ALL SELECT A.SLBH,A.CFLX AS DJLX,A.CFBH AS BDCZH,N''    AS ZSXLH,A.DJSJ AS DJRQ,A.LIFECYCLE,N'查封'  AS YWLX,A.GDH   from DJ_CF A LEFT JOIN DJ_TSGL B ON A.SLBH = B.SLBH LEFT JOIN DJ_XGDJGL C ON A.SLBH = C.ZSLBH WHERE A.XGZH IS NULL AND C.XGZH IS NULL AND C.FSLBH IS NULL) A LEFT JOIN DJ_TSGL B ON A.SLBH = B.SLBH LEFT JOIN DJ_SJD C ON A.SLBH = C.SLBH WHERE B.TSTYBM IS NOT NULL " + getWhere() + " order by A.Bdczh desc, A.SLBH) t"
       }
       console.log("data0", data);
       let config = {
         headers: {"Content-Type": "application/json"},
       };
 
+      // axios.post("http://127.0.0.1:8079/test/sqls", data, config).then((res) => {
       axios.post("http://127.0.0.1:8079/test/sqls", data, config).then((res) => {
         console.log("data", res.data.data);
         let datas = res.data.data.query;
         let str = '';
         for (var i = 0; i < datas.length; i++) {
 
-          str += `<tr class="load"><td style="width:60.17px; height:37px;">${datas[i].RN}</td><td style="width:117.34px; height:37px;">${datas[i].SLBH}</td><td style="width:61.17px; height:37px;">${datas[i].DJLX}</td><td style="width:174.52px; height:37px;">${datas[i].BDCZH}</td><td style="width:174.52px; height:37px;">${datas[i].BDCDYH}</td><td style="width:120.34px; height:37px;">${datas[i].ZSXLH}</td><td style="width:108.34px; height:37px;">${datas[i].DJRQ}</td><td style="width:60.17px; height:37px;">${datas[i].QLRMC}</td><td style="width:115.34px; height:37px;">${datas[i].QLRZJH}</td><td style="width:69.17px; height:37px;">${datas[i].LIFECYCLE}</td><td style="width:60.17px; height:37px;">${datas[i].GDH}</td><td style="width:185.17px; height:37px;">${datas[i].ZL}</td></tr>`
+          var QLRMC = GetQLRMC(datas[i].SLBH)
+
+          function GetQLRMC(sSLBH) {
+            var mc;
+            var sSql = {"sxkGETQLRMC": "SELECT A.QLRMC FROM DJ_QLR A LEFT JOIN DJ_QLRGL B ON A.QLRID = B.QLRID WHERE B.SLBH = '" + sSLBH + "' AND (B.QLRLX IS NULL OR B.QLRLX IN ('权利人', '抵押权人')) ORDER BY B.SXH"};
+            axios.post("http://127.0.0.1:8079/test/sqls", sSql, config).then((res) => {
+              console.log("qlrmc", res.data.data.sxkGETQLRMC[0]);
+              mc = res.data.data.sxkGETQLRMC[0].QLRMC;
+            })
+            return mc;
+          }
+
+          str += `<tr class="load"><td style="width:60.17px; height:37px;">${datas[i].RN}</td><td style="width:117.34px; height:37px;">${datas[i].SLBH}</td><td style="width:61.17px; height:37px;">${datas[i].DJLX}</td><td style="width:174.52px; height:37px;">${datas[i].BDCZH}</td><td style="width:174.52px; height:37px;">${datas[i].BDCDYH}</td><td style="width:120.34px; height:37px;">${datas[i].ZSXLH}</td><td style="width:108.34px; height:37px;">${datas[i].DJRQ}</td><td style="width:60.17px; height:37px;">${QLRMC}</td><td style="width:115.34px; height:37px;">${datas[i].QLRZJH}</td><td style="width:69.17px; height:37px;">${datas[i].LIFECYCLE}</td><td style="width:60.17px; height:37px;">${datas[i].GDH}</td><td style="width:185.17px; height:37px;">${datas[i].ZL}</td></tr>`
+
         }
+
+
         table.innerHTML = str;
         let tabkle = document.getElementsByClassName('load')
         document.addEventListener('click', function (e) {
@@ -151,44 +148,116 @@ const query = () => {
             for (let j = 0; j < 12; j++) {
               if (row.cells[j].innerHTML == e.target.innerHTML) {
                 tstybmdata.value = datas[i].TSTYBM;
-                zsdialogVisible.value = true;
-
-
-                var tree = {
-                  //加载证书信息
-                  loadCCIE: "SELECT DISTINCT B.TSTYBM,A.BDCZH,A.FZRQ,A.LIFECYCLE,A.DJRQ,A.YWLX,A.SLBH FROM (SELECT SLBH,BDCZH,FZRQ,DJRQ,LIFECYCLE,N'权证' AS YWLX FROM DJ_DJB UNION ALL SELECT SLBH,BDCZMH AS BDCZH,FZRQ,DJRQ,LIFECYCLE,N'预告' AS YWLX FROM DJ_YG) A LEFT JOIN DJ_TSGL B ON A.SLBH = B.SLBH LEFT JOIN DJ_SJD C ON A.SLBH = C.SLBH WHERE B.TSTYBM ='" + tstybmdata.value + "'  ORDER BY DJRQ DESC",
-                  //查询是否存在按单元查封信息:
-                  dycfxx: "SELECT COUNT(1) FROM DJ_CF A LEFT JOIN DJ_TSGL B ON A.SLBH = B.SLBH LEFT JOIN DJ_XGDJGL C ON A.SLBH = C.ZSLBH WHERE A.XGZH IS NULL AND C.XGZH IS NULL AND C.FSLBH IS NULL AND TSTYBM = '" + tstybmdata.value + "'",
-                  dydy: "SELECT COUNT(1) FROM DJ_DY A LEFT JOIN DJ_TSGL B ON A.SLBH = B.SLBH LEFT JOIN DJ_XGDJGL C ON A.SLBH = C.ZSLBH WHERE A.XGZH IS NULL AND C.XGZH IS NULL AND C.FSLBH IS NULL AND TSTYBM = '" + tstybmdata.value + "'"
-
-                }
-                axios.post("http://127.0.0.1:8079/test/sqls", tree, config).then((res) => {
-                  console.log("zszs", res.data.data);
-                  if (res.data.data.loadCCIE != null) {
-
-                  }
-
-                })
-
+                loadTreeInfo(tstybmdata.value);
 
               }
             }
           }
-          console.log('TSTYBM  ', tstybmdata.value)
         })
 
       })
     }
-
-    function LoadTreeInfo(TSTYBM) {
-      console.log("ts", TSTYBM)
-    }
-
   }, 1000)
 
-};
+  function loadTreeInfo(TSTYBM) {
 
 
+    var treeSqls = {
+      //加载证书信息
+      loadCCIE: "SELECT DISTINCT B.TSTYBM,A.BDCZH,A.FZRQ,A.LIFECYCLE,A.DJRQ,A.YWLX,A.SLBH FROM (SELECT SLBH,BDCZH,FZRQ,DJRQ,LIFECYCLE,N'权证' AS YWLX FROM DJ_DJB UNION ALL SELECT SLBH,BDCZMH AS BDCZH,FZRQ,DJRQ,LIFECYCLE,N'预告' AS YWLX FROM DJ_YG) A LEFT JOIN DJ_TSGL B ON A.SLBH = B.SLBH LEFT JOIN DJ_SJD C ON A.SLBH = C.SLBH WHERE B.TSTYBM ='" + TSTYBM + "'  ORDER BY DJRQ DESC",
+      //查询是否存在按单元查封信息:
+      dycfxx: "SELECT COUNT(1) AS COUNT FROM DJ_CF A LEFT JOIN DJ_TSGL B ON A.SLBH = B.SLBH LEFT JOIN DJ_XGDJGL C ON A.SLBH = C.ZSLBH WHERE A.XGZH IS NULL AND C.XGZH IS NULL AND C.FSLBH IS NULL AND TSTYBM = '" + TSTYBM + "'",
+      //加载按单元抵押信息
+      dydyxx: "SELECT COUNT(1) AS COUNT FROM DJ_DY A LEFT JOIN DJ_TSGL B ON A.SLBH = B.SLBH LEFT JOIN DJ_XGDJGL C ON A.SLBH = C.ZSLBH WHERE A.XGZH IS NULL AND C.XGZH IS NULL AND C.FSLBH IS NULL AND TSTYBM = '" + TSTYBM + "'"
+
+    }
+    let config = {
+      headers: {"Content-Type": "application/json"},
+    };
+    var ArrQZXX = new Array();
+    axios.post("http://127.0.0.1:8079/test/sqls", treeSqls, config).then((res) => {
+      console.log("zszs", res.data.data);
+      var zs = res.data.data.loadCCIE;
+      console.log("loadCCIE", zs);
+      if (zs != null) {
+        var treedata = [];
+        for (var i = 0; i < zs.length; i++) {
+          var zsTSTYBM = zs[i].TSTYBM;
+          var zsSLBH = zs[i].SLBH;
+          var sKeyName = zsSLBH + ";" + zsTSTYBM;
+          var zsBDCZH = zs[i].BDCZH;
+          var zsLIFECYCLE = zs[i].LIFECYCLE;
+          var zsDJRQ = zs[i].DJRQ;
+          var zsYWLX = zs[i].YWLX
+          if (zsBDCZH == "" || zsBDCZH == null) {
+            zsBDCZH = zsSLBH;
+          }
+          var zsDQXX = "";
+          if (zsYWLX == "预告") {
+            zsDQXX += "预告-";
+          }
+          zsDQXX += zsBDCZH;
+          if (zsLIFECYCLE == "1") {
+            zsDQXX += "(历史)";
+          } else if (zsDJRQ == "" || zsDJRQ == null) {
+            zsDQXX += "(办理中)";
+          } else {
+            zsDQXX += "(现实)";
+          }
+          ArrQZXX[i] = sKeyName;
+          console.log("zsxx", zsDQXX);
+          treedata.push(getTreeNode(zsDQXX))
+        }
+
+        if (res.data.data.dycfxx[0].COUNT > 0) {
+          treedata.push(getTreeNode("按单元查封信息"));
+        }
+        if (res.data.data.dydyxx[0].COUNT > 0) {
+          treedata.push(getTreeNode("按单元抵押信息"));
+        }
+        //加载限制性登记信息
+        for (let i = 0; i < ArrQZXX.length; i++) {
+          let arrQZXX = ArrQZXX[i];
+          let split = arrQZXX.split(";");
+
+        }
+
+        console.log("ArrQZXX", ArrQZXX)
+
+        treeData.value = treedata
+        console.log("treeData", treeData);
+        setTimeout(() => {
+          zsdialogVisible.value = true;
+        }, 1000);
+
+      }
+
+
+    })
+
+    function getTreeNode(zsDQXX) {
+      return {
+        label: zsDQXX,
+        icon: 'iconfont icon-wenjian',
+        style: "color:#ffe090;font-size: 23px",
+        detail: "",
+        lcmc: "",
+        lcjson: "",
+        key: "",
+      }
+    }
+
+//格式化
+    function Format(sVal) {
+      if (sVal == null || sVal + "" == "null" || typeof (sVal) == "undefined") {
+        return "";
+      } else {
+        return "" + sVal;
+      }
+    }
+
+  }
+}
 onMounted(() => {
   zs();
   // fromname();
